eabeb43f61a0e5b6620fba80eacb3940df85f5bc
thiago-salaberry <thiagosalaberry08@gmail.com>
2024-09-24 12:29:31 -0300

    parte 35.3

--- java/img_cambio.js
+++ java/img_cambio.js
@@ -2,7 +2,108 @@ function cambiarImagenAjolote(nombre) {
     const imagen = document.getElementById('imagen-ajolote');
     imagen.src = `../imagenes/${nombre}.png`;
 }
+
 function cambiarImagenAldeano(nombre) {
     const imagen = document.getElementById('imagen-aldeano');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenArmadillo(nombre) {
+    const imagen = document.getElementById('imagen-armadillo');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenBurro(nombre) {
+    const imagen = document.getElementById('imagen-burro');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenCaballo(nombre) {
+    const imagen = document.getElementById('imagen-caballo');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenCaballoEsqueleto(nombre) {
+    const imagen = document.getElementById('imagen-caballo-esqueleto');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenCamello(nombre) {
+    const imagen = document.getElementById('imagen-camello');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenCerdo(nombre) {
+    const imagen = document.getElementById('imagen-cerdo');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenChampivaca(nombre) {
+    const imagen = document.getElementById('imagen-champivaca');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenConejo(nombre) {
+    const imagen = document.getElementById('imagen-conejo');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenGato(nombre) {
+    const imagen = document.getElementById('imagen-gato');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenGolemDeNieve(nombre) {
+    const imagen = document.getElementById('imagen-golem-de-nieve');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenStrider(nombre) {
+    const imagen = document.getElementById('imagen-strider');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenLoro(nombre) {
+    const imagen = document.getElementById('imagen-loro');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenMula(nombre) {
+    const imagen = document.getElementById('imagen-mula');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenMurcielago(nombre) {
+    const imagen = document.getElementById('imagen-murcielago');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenOcelote(nombre) {
+    const imagen = document.getElementById('imagen-ocelote');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenOveja(nombre) {
+    const imagen = document.getElementById('imagen-oveja');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenPezGlobo(nombre) {
+    const imagen = document.getElementById('imagen-pez-globo');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenPollo(nombre) {
+    const imagen = document.getElementById('imagen-pollo');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenRana(nombre) {
+    const imagen = document.getElementById('imagen-rana');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenVaca(nombre) {
+    const imagen = document.getElementById('imagen-vaca');
     imagen.src = `../imagenes/${nombre}.png`;
 }
