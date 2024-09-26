82836c5dbe0bf838446d178f6fb5da21058624a3
thiago-salaberry <thiagosalaberry08@gmail.com>
2024-09-26 12:01:19 -0300

    parte 35.5

--- java/img_cambio.js
+++ java/img_cambio.js
@@ -1,3 +1,4 @@
+//mobs pasivos
 function cambiarImagenAjolote(nombre) {
     const imagen = document.getElementById('imagen-ajolote');
     imagen.src = `../imagenes/${nombre}.png`;
@@ -102,9 +103,9 @@ function cambiarImagenRana(nombre) {
     const imagen = document.getElementById('imagen-rana');
     imagen.src = `../imagenes/${nombre}.png`;
 }
-
-function cambiarImagenAhogado(nombre) {
-    const imagen = document.getElementById('imagen-ahogado');
+//mobs neutrales
+function cambiarImagenCabra(nombre) {
+    const imagen = document.getElementById('imagen-cabra');
     imagen.src = `../imagenes/${nombre}.png`;
 }
 
@@ -113,12 +114,190 @@ function cambiarImagenVaca(nombre) {
     imagen.src = `../imagenes/${nombre}.png`;
 }
 
-function cambiarImagenVaca(nombre) {
-    const imagen = document.getElementById('imagen-vaca');
+function cambiarImagenEnderman(nombre) {
+    const imagen = document.getElementById('imagen-enderman');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+
+function cambiarImagenGolemHierro(nombre) {
+    const imagen = document.getElementById('imagen-golem-hierro');
     imagen.src = `../imagenes/${nombre}.png`;
 }
 
-function cambiarImagenVaca(nombre) {
-    const imagen = document.getElementById('imagen-vaca');
+function cambiarImagenLlama(nombre) {
+    const imagen = document.getElementById('imagen-llama');
     imagen.src = `../imagenes/${nombre}.png`;
 }
+
+function cambiarImagenLobo(nombre) {
+    const imagen = document.getElementById('imagen-lobo');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenOsoPolar(nombre) {
+    const imagen = document.getElementById('imagen-oso-polar');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenPanda(nombre) {
+    const imagen = document.getElementById('imagen-panda');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenPiglin(nombre) {
+    const imagen = document.getElementById('imagen-piglin');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+
+function cambiarImagenPiglinZombificado(nombre) {
+    const imagen = document.getElementById('imagen-piglin-zombificado');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenZorro(nombre) {
+    const imagen = document.getElementById('imagen-zorro');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+//mobs hostiles
+function cambiarImagenAldeanoZombi(nombre) {
+    const imagen = document.getElementById('imagen-aldeano-zombi');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenBlaze(nombre) {
+    const imagen = document.getElementById('imagen-blaze');
+    imagen.src = `../imagenes/${nombre}.gif`;
+}
+
+function cambiarImagenBruja(nombre) {
+    const imagen = document.getElementById('imagen-bruja');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenCienago(nombre) {
+    const imagen = document.getElementById('imagen-cienago');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+
+function cambiarImagenCreeper(nombre) {
+    const imagen = document.getElementById('imagen-creeper');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+
+function cambiarImagenCuboDeMagma(nombre) {
+    const imagen = document.getElementById('imagen-cubo-de-magma');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+
+function cambiarImagenDisecado(nombre) {
+    const imagen = document.getElementById('imagen-disecado');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenErrante(nombre) {
+    const imagen = document.getElementById('imagen-errante');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenEsqueleto(nombre) {
+    const imagen = document.getElementById('imagen-esqueleto');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenEsqueletoDelWither(nombre) {
+    const imagen = document.getElementById('imagen-esqueleto-del-wither');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenGhast(nombre) {
+    const imagen = document.getElementById('imagen-ghast');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+
+function cambiarImagenGuardian(nombre) {
+    const imagen = document.getElementById('imagen-guardian');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+function cambiarImagenGuardianAnciano(nombre) {
+    const imagen = document.getElementById('imagen-guardian-anciano');
+    if (nombre.endsWith('.gif')) {
+        imagen.src = `../imagenes/${nombre}`;
+    } else {
+        imagen.src = `../imagenes/${nombre}.png`;
+    }
+}
+
+function cambiarImagenHoglin(nombre) {
+    const imagen = document.getElementById('imagen-hoglin');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenInvocador(nombre) {
+    const imagen = document.getElementById('imagen-invocador');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenPiglinBruto(nombre) {
+    const imagen = document.getElementById('imagen-piglin-bruto');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenSaqueador(nombre) {
+    const imagen = document.getElementById('imagen-saqueador');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenShulker(nombre) {
+    const imagen = document.getElementById('imagen-shulker');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenVex(nombre) {
+    const imagen = document.getElementById('imagen-vex');
+    imagen.src = `../imagenes/${nombre}.gif`;
+}
+
+function cambiarImagenVindicador(nombre) {
+    const imagen = document.getElementById('imagen-vindicador');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenZoglin(nombre) {
+    const imagen = document.getElementById('imagen-zoglin');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+function cambiarImagenZombi(nombre) {
+    const imagen = document.getElementById('imagen-zombi');
+    imagen.src = `../imagenes/${nombre}.png`;
+}
+
+
