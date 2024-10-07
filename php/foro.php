fb5674e080d7bfffbed04caf7cfb9f45e7a2ab62
thiago-salaberry <thiagosalaberry@gmail.com>
2024-10-07 16:56:21 -0300

    parte 38

--- php/foro.php
+++ php/foro.php
@@ -1,3 +1,6 @@
+<?php
+    require 'sistema.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +9,26 @@
     <title>Foro de imagenes</title>
     <link rel="stylesheet" href="../css/foro.css">
 </head>
-<body>
-    
-</body>
+<main>
+    <h1>Foro de la wiki</h1>
+    <section>
+        <h3>¿quiere insertar su logro?</h3>
+        <form action="publicar_fotos" method="post">
+            <div>
+                <label for="img">Selecione una imagen:</label>
+                <input id='imagen' type="file" name='foto'>
+            </div>
+            <div>
+                <label for="descripcion">Descripcion:</label>
+                <input type="text" name='nombre' required>
+            </div>
+            <div>
+                <button type="submit" class="btn">subir</button>
+            </div>
+        </form>
+    </section>
+    <section>
+        <h3>publicaciones</h3>
+    </section>
+</main>
 </html>
