d54a81c8fec141bfb7eca410d74f1e905d921136
thiago-salaberry <thiagosalaberry08@gmail.com>
2024-10-08 10:30:10 -0300

    parte 38.1

--- php/foro.php
+++ php/foro.php
@@ -13,7 +13,7 @@
     <h1>Foro de la wiki</h1>
     <section>
         <h3>¿quiere insertar su logro?</h3>
-        <form action="publicar_fotos" method="post">
+        <form action="publicar_fotos" method="post" enctyper="multipart/form-data">
             <div>
                 <label for="img">Selecione una imagen:</label>
                 <input id='imagen' type="file" name='foto'>
@@ -29,6 +29,23 @@
     </section>
     <section>
         <h3>publicaciones</h3>
+        <?php
+            if ($result->num_rows > 0) {
+                // Mostrar cada imagen
+                while ($row = $result->fetch_assoc()) {
+                    echo '<div>';
+                    echo '<h3>' . htmlspecialchars($row['nombre'] . ' ' . $row['apellido']) . '</h3>'; // Mostrar el nombre del usuario
+                    echo '<p>' . htmlspecialchars($row['descripcion']) . '</p>'; // Mostrar la descripción
+                    echo '<img src="' . htmlspecialchars($row['ruta_imagen']) . '" alt="Imagen" style="max-width: 300px; height: auto;"/>'; // Mostrar la imagen
+                    echo '</div><hr>';
+        }
+    } else {
+        echo "<p>No hay imágenes disponibles.</p>";
+    }
+
+    $stmt_imagenes->close();
+    $conn->close();
+    ?>
     </section>
 </main>
 </html>
